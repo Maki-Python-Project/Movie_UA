356dc242dae18eb8e2de5b78c7f2401b926eb5cb
Maki <Maki@gmail.com>
2021-09-14 22:33:48 +0300

    ua

---
+++ movies/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.forms import fields
+
+from .models import Reviews, Rating, RatingStar
+
+class ReviewForm(forms.ModelForm):
+    
+    class Meta:
+        model = Reviews
+        fields = ("name", "email", "text")
+
+class RatingForm(forms.ModelForm):
+
+    star = forms.ModelChoiceField(
+        queryset = RatingStar.objects.all(), widget = forms.RadioSelect(), empty_label = None
+    )
+
+    class Meta:
+        model = Rating
+        fields = ('star',)
